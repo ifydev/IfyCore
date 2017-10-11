584022d832a11a7acce69cf1c98e08094831db92
Innectic <innectic@gmail.com>
2017-10-11 13:23:36 -0700

    Fix enchantment registry never being setup

--- Spigot/src/main/java/me/ifydev/core/spigot/Core.java
+++ Spigot/src/main/java/me/ifydev/core/spigot/Core.java
@@ -1,7 +1,6 @@
 package me.ifydev.core.spigot;
 
-import lombok.Getter;
-import me.ifydev.core.api.CoreAPI;
+import me.ifydev.core.spigot.enchant.EnchantRegistry;
 import me.ifydev.core.spigot.items.BlockRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,6 +13,7 @@ public class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         BlockRegistry.setup();
+        EnchantRegistry.setup();
     }
 
     @Override
